7985f714f33be77f66d71943ec57fe37b2e65ce3
adsznzhang <adszn@hotmail.com>
2017-04-18 21:37:19 +0800

    Style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var React = require('react');
 // var About = React.createClass({
 //     render: function() {
 //         return(
-//                 <h3>About Component</h3>
+//                 <h1>About Component</h1>
 //         );
 //     }
 // });
@@ -12,8 +12,18 @@ var React = require('react');
 var About = (props) => {
     return(
         <div>
-        <h3>About Component</h3>
+        <h1 className="text-center">About Component</h1>
         <p>Welcome to the about page</p>
+            <p>
+            </p>
+            <ul>
+            <li>
+            <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+            </li>
+            <li>
+            <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+            </li>
+            </ul>
         </div>
     )
 };
